7266f9c05d45515bc91ec4c1ac2e31d73fa44783
Monica Navarro <me.navarro.97@outlook.com>
2022-06-27 18:02:10 +0100

    User can choose rock, paper or scissors and get confirmation of their move, test passing

--- spec/feature/game_spec.rb
+++ spec/feature/game_spec.rb
@@ -6,9 +6,15 @@ feature 'playing a game' do
     fill_in 'name', with: 'Monica'
     click_button 'Submit'
   end
+
   scenario 'see choices' do
-    expect(page).to have_content 'Rock'
-    expect(page).to have_content 'Paper'
-    expect(page).to have_content 'Scissors'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
+  end
+
+  scenario 'choose rock, paper or scissors' do
+    click_button 'Rock' 
+    expect(page).to have_content "You chose Rock!"
   end
 end
